d15bcb814ad34d3618279b48aced618291022d63
Cláudio Gomes <cla.gomess@gmail.com>
2020-03-06 20:57:00 -0300

    esqueleto

---
+++ src/main/java/com/github/clagomess/unboxmoviedb/UnboxmoviedbApplication.java
@@ -0,0 +1,13 @@
+package com.github.clagomess.unboxmoviedb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnboxmoviedbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnboxmoviedbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/clagomess/unboxmoviedb/UnboxmoviedbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.clagomess.unboxmoviedb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnboxmoviedbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
