724bd20f292f7987b5f8ef24348ae9d9aa768d27
Claudio Gomes <cla.gomess@gmail.com>
2020-09-21 02:16:09 -0300

    swagger doc

--- src/main/java/com/github/clagomess/unboxmoviedb/controller/FilmeController.java
+++ src/main/java/com/github/clagomess/unboxmoviedb/controller/FilmeController.java
@@ -3,6 +3,7 @@ package com.github.clagomess.unboxmoviedb.controller;
 import com.github.clagomess.unboxmoviedb.dto.filme.FilmeVotoDto;
 import com.github.clagomess.unboxmoviedb.entity.Filme;
 import com.github.clagomess.unboxmoviedb.service.FilmeService;
+import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 public class FilmeController {
     private final FilmeService filmeService;
 
+    @ApiOperation(value = "Busca informções de um filme pelo ID")
     @GetMapping("{seqFilme}")
     public Filme getDetails(
             @PathVariable("seqFilme") Long seqFilme
@@ -21,6 +23,8 @@ public class FilmeController {
         return filmeService.findById(seqFilme);
     }
 
+    @ApiOperation(value = "Realiza o voto")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
     @PostMapping("rating")
     public ResponseEntity<?> voto(@RequestBody FilmeVotoDto dto) throws Exception {
         filmeService.manterVoto(dto);
